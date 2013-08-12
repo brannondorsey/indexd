bb7ac5a6b04110e32a872ba371a1ec27a5086ed6
Kevin Zweerink <kevin@kevinzweerink.com>
2013-08-11 19:51:23 -0700

    add form/input to bookmark button

--- listing.php
+++ listing.php
@@ -34,7 +34,9 @@
         <section class="listings">
             <section class="query">
                 <div class="profile">
-                    <a class="bookmark false">b</a>
+                    <form action="" method="post" id="bookmark-form">
+                        <input type="submit" id="bookmark-toggle" class="bookmark false" value="b">
+                    </form>
                     <h2><?php echo $profile_data->first_name . " " . $profile_data->last_name; ?></h2>
                     <div class="media">
                     <?php 
